757341492e2a44ec6c52f4e01c261154c645aa43
WuTUT <762641171@qq.com>
2017-10-10 23:46:14 +0800

    add env

--- main.cpp
+++ main.cpp
@@ -1,8 +1,12 @@
-#include <QCoreApplication>
-
+#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
+#include <opencv2/core/core.hpp>
+#include <opencv2/objdetect/objdetect.hpp>
+#include <QDebug>
+#include <iostream>
+using namespace cv;
+using namespace std;
 int main(int argc, char *argv[])
 {
-    QCoreApplication a(argc, argv);
-
-    return a.exec();
+   return 0;
 }
